421daee409b4a9b82253c891cc2392aac4fdfef2
dileepumar <chokkakulakumar1996@gmail.com>
2024-06-21 11:23:34 +0530

    student

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -4,20 +4,44 @@ import java.util.Scanner;
 
 public class StudentGradeEvaluation {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
         // Prompt the user to enter their name
         System.out.print("Enter your name: ");
-        
+        String name=s.nextline();
 
         // Prompt the user to enter their age
-        
+        System.out.println("Enter your age: ");
+        int Age=s.nextInt();
+
 
         // Prompt the user to enter their exam score
-        
+        System.out.println("Enter your exam score: ");
+        int score=s.nextInt();
+        System.out.println("name: "+name);
+        System.out.println("age: "+Age);
+        System.out.println("Score: "+score);
 
         // Determine the grade
         String grade;
+        if(score>=90)
+        {
+            System.out.println("Grade: A");
+        }
+        else if(score>=80&&score<89) {
+            System.out.println("grade: B")
+            
+        }
+        else if(score>=70&&score<79){
+            System.out.println("grade: C");
+        }  
+        else if(score>=600&&score<69){
+            System.out.println("grade: D");
+        } 
+        else   
+        {
+            System.out.println("Fail");
+        }
         
 
         // Print the student's details
